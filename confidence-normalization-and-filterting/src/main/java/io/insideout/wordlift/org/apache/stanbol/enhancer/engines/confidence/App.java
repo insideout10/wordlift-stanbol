32179d433bdaad22e1c5c6cccc7f35cc42362869
David Riccitelli <ziodave@gmail.com>
2012-08-30 09:28:17 +0300

    add a new project for a confidence normalization and filtering engine

---
+++ confidence-normalization-and-filterting/src/main/java/io/insideout/wordlift/org/apache/stanbol/enhancer/engines/confidence/App.java
@@ -0,0 +1,13 @@
+package io.insideout.wordlift.org.apache.stanbol.enhancer.engines.confidence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
